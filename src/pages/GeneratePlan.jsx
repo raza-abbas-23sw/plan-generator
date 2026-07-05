185927016be32ede98fd6b91b75693fd65ca4385
Raza Abbas <157038022+raza-abbas-23sw@users.noreply.github.com>
2026-07-05 17:22:18 +0500

    update expected maturity

--- src/pages/GeneratePlan.jsx
+++ src/pages/GeneratePlan.jsx
@@ -858,7 +858,8 @@ let formattedBonus = format(_50_Percent_Bonus/2);
               }}
             >
               Expected Maturity Return is Rs.{" "}
-              {format(insuranceResults[3].value * ((insuranceResults[2].value=='Endowment (10 Years)'||insuranceResults[2].value=="Platinium (10 Years)")? 2: 4.25))}
+              {format(insuranceResults[3].value * ((insuranceResults[2].value=='Endowment (10 Years)'||insuranceResults[2].value=="Platinium (10 Years)")? 2.5: 4.5))}
+              Plus 
             </h1>
 
             <h3
